e6406b2693f96e31703e5bc82b617e062e04478b
Biowitt <79954143+Biowitt@users.noreply.github.com>
2022-08-11 14:38:11 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Sepehr Haghighat" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to my page <code></code>
         </p>
       </main>
 
